c0c976c3eb02b1ddedd415357cb337ee37205c75
Anthony-byte-bit <Amp139929@gmail.com>
2024-11-02 09:00:46 -0500

    Add files via upload

---
+++ component.formulario.js
@@ -0,0 +1,48 @@
+const Formulario = () => {
+  return (
+    <form className="p-6 bg-yellow shadow-md rounded-lg space-y-4">
+      <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+        Nombre:
+      </label>
+      <input
+        type="text"
+        id="name"
+        placeholder="Anthony Mondalgo"
+        className="w-full p-2 border border-gray-300 rounded"
+      />
+
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Email:
+      </label>
+      <input
+        type="email"
+        id="email"
+        placeholder="futsite@gmail.com"
+        className="w-full p-2 border border-gray-300 rounded"
+      />
+
+      <label
+        htmlFor="message"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Mensaje:
+      </label>
+      <input
+        type="text"
+        id="message"
+        placeholder="Escríbenos..."
+        className="w-full p-2 border border-gray-300 rounded"
+      ></input>
+
+      <button
+        value="button"
+        className="w-full py-2 bg-yellow-500 text-white rounded hover:bg-yellow-700"
+      >
+        ENVIAR
+      </button>
+    </form>
+  );
+};
---
+++ filter.js
@@ -0,0 +1,15 @@
+const Filter = ({ onFilterChange }) => {
+  return (
+    <select
+      onChange={(e) => onFilterChange(e.target.value)}
+      className="p-2 border rounded bg-yellow-500 text-white my-4 flex text-center object-center w-80 text-xl"
+    >
+      <option value="all">Todo</option>
+      <option value="barcelona">FC Barcelona</option>
+      <option value="real madrid">Real Madrid</option>
+      <option value="manchester city">Manchester City</option>
+      <option value="inter miami">Inter Miami</option>
+      <option value="seleccion peruana">Selección peruana</option>
+    </select>
+  );
+};
